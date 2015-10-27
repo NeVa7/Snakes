38d251fe96bc64bda7aa770366189627f109e502
NeVa7 <st_vich_nv@mail.ru>
2015-10-27 23:18:22 +0300

    создание конструктора

--- Snakes/Program.cs
+++ Snakes/Program.cs
@@ -11,11 +11,15 @@ namespace Snakes
         static void Main(string[] args)
         {
 
-            Point p1 = new Point();
-            p1.x= 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1,3,'*');
             p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            Point p3 = new Point(6, 7, '@');
+            p3.Draw();
+
             Console.ReadLine();
         
         }
