30c2a3c40b3d7f63f7fe7343489cfb28ebfe4cb2
NeVa7 <st_vich_nv@mail.ru>
2015-10-27 23:08:38 +0300

    создание конструктора (reverted from commit 622727547ab24617c52c3dd7accf8bc4945ba82b)

--- Snakes/Point.cs
+++ Snakes/Point.cs
@@ -11,13 +11,7 @@ namespace Snakes
         public int x;
         public int y;
         public char sym;
-
-        public Point(int _x, int _y, char _sym)
-        {
-            x = _x;
-            y = _y;
-            sym = _sym;
-        }
+        
 
         public void Draw()
         {
